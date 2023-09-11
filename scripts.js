9a55014794d42042af0c5a55322ca472a24972bd
John Dungao <johnmiguel.dungao@gmail.com>
2023-09-11 19:36:30 -0400

    create a board

---
+++ scripts.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * TODO:
+ *  - Create a board
+ *  - Create a player
+ *  - Able to switch players
+ *  - Place a mark on the board
+ *  - Check for a winner
+ *      - Display the winner
+ *  - Check for a tie
+ *      - Display the tie
+ *  - Reset the game
+ *      - Clear the board
+ *  - Display the current player
+    
+ *
+ *
+ *
+ */
+
+const board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+document.querySelectorAll(".cell");
