51d5300f5ecd7a3628482a40b8bc078f1ce1ad2d
John Dungao <johnmiguel.dungao@gmail.com>
2023-09-12 17:04:45 -0400

    add tie logic

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@
 // State
 let turn = true;
 let player = "X";
+let tries = 9;
 
 // Create a board
 const board = [
@@ -41,7 +42,7 @@ for (let i = 0; i < cells.length; i++) {
 
 // Add event listener to reset button
 const resetBtn = document.querySelector("#btn-reset");
-resetBtn.addEventListener("click", resetBoard);
+resetBtn.addEventListener("click", resetBoardUI);
 
 /********** FUNCTIONS ****************/
 
@@ -64,50 +65,76 @@ function changeBoardState(idx) {
   }
 }
 
+// Reset Board State
+function resetBoardState() {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      board[i][j] = "";
+    }
+  }
+  tries = 9;
+}
+
 // Function mark the board
 function handleClick(evt) {
   turn ? (player = "X") : (player = "O");
   evt.target.textContent = player;
 
+  tries--;
   // Change the board state
   changeBoardState(Number(evt.target.dataset.index));
+  // Change Turn
+  turn = !turn;
   // Check for Winner
-  if (checkForWinner(player)) alert(`Player ${player} won!`);
+  if (checkForWinner(player)) {
+    document.getElementById("player-label").innerHTML = `Player ${player} won!`;
+    resetBoardState();
+  }
+
+  // Check for Tie
+  if (tries == 0) {
+    document.getElementById("player-label").innerHTML = "It's a TIE!";
+  }
 
   // Remove Event Listener of clicked cells
   evt.target.removeEventListener("click", handleClick);
-  //Change Turn
-  turn = !turn;
 }
 
 // Reset the board
-function resetBoard() {
+function resetBoardUI() {
   const cells = document.querySelectorAll(".cell");
   for (let i = 0; i < cells.length; i++) {
     cells[i].textContent = "";
     cells[i].addEventListener("click", handleClick);
   }
+  // Reset Board
+  resetBoardState();
+  document.getElementById("player-label").textContent = "";
 }
 
 // check for a winner
 function checkForWinner(player) {
   // Check for winner Diagonally
   if (player == board[0][0] && player == board[1][1] && player == board[2][2]) {
+    console.log("Won from diagonal 1");
     return true;
   }
   if (player == board[0][2] && player == board[1][1] && player == board[2][0]) {
+    console.log("Won from diagonal 2");
     return true;
   }
 
   //Check for Horizontal Winner
   for (const val of board) {
     if (player == val[0] && player == val[1] && player == val[2]) {
+      console.log("Hortizontal");
       return true;
     }
   }
   // Check for Vertical Winner
   for (const i in board) {
     if (player == board[0][i] && player == board[1][i] && board[2][i]) {
+      console.log("Vertical");
       return true;
     }
   }
